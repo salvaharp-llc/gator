d59b1f62b14d5230376bf5e25eec9da44b8b37bc
salvaharp-llc <salvaharp@gmail.com>
2025-10-22 19:34:42 +0200

    add feeds listing

--- handle_feed.go
+++ handle_feed.go
@@ -33,11 +33,38 @@ func handlerAddFeed(s *state, cmd command) error {
 	}
 
 	fmt.Println("Feed created successfully:")
-	fmt.Printf("Id: %v\nName: %s\nURL: %s\nUserID: %v\n",
+	fmt.Printf("- Id: %v\n- Name: %s\n- URL: %s\n- UserID: %v\n",
 		feed.ID,
 		feed.Name,
 		feed.Url,
 		feed.UserID,
 	)
+	return nil
+}
+
+func handlerListFeeds(s *state, cmd command) error {
+	if len(cmd.args) != 0 {
+		return fmt.Errorf("usage: %s", cmd.name)
+	}
+
+	feeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return fmt.Errorf("could not retrieve feeds: %w", err)
+	}
+
+	if len(feeds) == 0 {
+		fmt.Println("No feeds found.")
+		return nil
+	}
+
+	fmt.Printf("Found %d feeds:\n", len(feeds))
+	for i, feed := range feeds {
+		user, err := s.db.GetUserById(context.Background(), feed.UserID)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Feed %d:\n", i)
+		fmt.Printf("- Name: %s\n- URL: %s\n- User: %s\n", feed.Name, feed.Url, user.Name)
+	}
 	return nil
 }
--- main.go
+++ main.go
@@ -41,6 +41,7 @@ func main() {
 	cmds.register("users", handlerListUsers)
 	cmds.register("agg", handlerAggregation)
 	cmds.register("addfeed", handlerAddFeed)
+	cmds.register("feeds", handlerListFeeds)
 
 	args := os.Args
 	if len(args) < 2 {
